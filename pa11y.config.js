2c2e6436339efb75169e917cbd931f655ed67b52
Peter Hunt <naseberry@users.noreply.github.com>
2017-03-06 15:40:18 +0000

    Manual url test branch

--- pa11y.config.js
+++ pa11y.config.js
@@ -2,26 +2,19 @@ var readDir = require('readdir');
 
 
 exports.standard = 'WCAG2AA'; // set test standards
-exports.ignore = ['warning', 'notice']; // ignore WCAG warnings and notices
+// exports.ignore = ['warning', 'notice']; // ignore WCAG warnings and notices
 // Log what's happening to the console
 exports.log = {
-  debug: console.log.bind(console),
-  error: console.error.bind(console),
+  // debug: console.log.bind(console),
+  // error: console.error.bind(console),
   info: console.log.bind(console)
 };
 exports.concurrency = '10'; // set the concurrency here to run more tests in parallel
 
 
-function getFiles(){
-  const arr = [];
-  const filePath = 'file://' + process.cwd() + '/';
-  const files = readDir.readSync( './', ['**.html'] );
-  for (var i = 0; i < files.length; i++) {
-    arr.push(filePath + files[i]);
-  }
-  return arr;
-}
-exports.urls = getFiles();
+exports.urls = [
+  "https://www.parliament.uk/"
+];
 
 
 // debug: check module output
